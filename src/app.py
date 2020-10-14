2019dd33d3118c5a7d4dd774c68983acd37f8c71
Naila <nailakaliyeva@gmail.com>
2020-10-14 18:26:07 +0000

    fixed the json file took it out of loop

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ API_KEY = os.getenv("API_KEY")
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 #print("Hello, {}".format(input("What is your name?")))
 #words = input("What term do you want to look for?").split(",")
+cache = {}                                           #so this is the way to store information in a dict
+cache["definitions"] = []
 list_sys = sys.argv[1:]
 for i in range(0,len(list_sys)):
     word = list_sys[i]
@@ -34,18 +36,19 @@ for i in range(0,len(list_sys)):
     the_definition = "".join([i for i in data["list"][0]["definition"] if i != "[" and i!="]"])
                                                         
     #creating the dict cache after assigning value to the_definition, otherwise it'll be undefined
-    cache = {}                                           #so this is the way to store information in a dict
-    cache["definitions"] = []                            
+                                
     cache["definitions"].append({word: the_definition})  
+    
 
 
-    if word in cache["definitions"][0].keys():  #need to ask Paolo how to test  lines 37 & 38, cuz I think it's skipping them
+    if word in cache["definitions"][0].keys():  #need to ask Paolo how to test  lines 42 & 43, cuz I think it's skipping them
         print(word.capitalize()+ " is "+ cache["definitions"][0][word]) #since the app only asks for words only when you
     else:                                                               #run "pipenv run python src/app.py"
         print(word.capitalize() + " is "+ the_definition) 
 
-
+    
 
     #this is how to store new data in a json file
-    with open("json_file.json", "w+") as f:                 
-        json.dump(cache, f)
+with open("json_file.json", "a") as f:                 
+    json.dump(cache, f)
+    
